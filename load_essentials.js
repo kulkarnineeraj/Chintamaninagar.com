79f07369525797adfc61c8055f77fefe01874597
Neeraj <neeraj.kulkarni@siemens.com>
2020-10-05 22:15:29 +0530

    firstcode

---
+++ load_essentials.js
@@ -0,0 +1,15 @@
+document.getElementById("myHead").innerHTML =
+	"<span id='headerText'>VIT college</span>"
+	+ "<span id='headerSubtext'>Put logo and other things here</span>";
+document.getElementById("myNav").innerHTML =
+	"<ul id='navLinks'>"
+	+ "<li><a href='index.html'>Home</a></li>"
+	+ "<li><a href='about.html'>About</a>"
+	+ "<li><a href='donate.html'>Donate</a></li>"
+	+ "</ul>";
+document.getElementById("myFooter").innerHTML =
+	"<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + " You. All"
+	+ " rights reserved.</p>"
+	+ "<p id='credits'>Layout by You</p>"
+	+ "<p id='contact'><a href='mailto:you@you.com'>Contact Us</a> / "
+	+ "<a href='mailto:you@you.com'>Report a problem.</a></p>";
